766b8c80d2e8e011b635c1e332bb2775cfb3807b
Igor da Silva Dias <rogigor1712@gmail.com>
2026-01-05 17:47:17 -0300

    fake-terminal V1.0

---
+++ app/terminal.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Terminal() {
+    return (
+        <div>
+            <div className="terminal-wrapper">
+                <textarea spellCheck='false' ></textarea>
+            </div>
+        </div>
+    )
+}
